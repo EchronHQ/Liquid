afe0ded82856f9e9e80c889279ccec317ced2153
Stijn Duynslaeger <stijn@echron.com>
2023-11-10 20:33:25 +0000

    Update index and cli files

--- app/pub/index.php
+++ app/pub/index.php
@@ -6,12 +6,14 @@ use Liquid\Core\Application;
 try {
     require __DIR__ . '/../app/bootstrap.php';
 
-    $app = new Application();
+    $app = new Application(\ROOT);
     $app->run();
-} catch (Throwable $ex) {
+} catch (\Throwable $ex) {
     // TODO: only show this in developer mode
+
     echo '<div>' . $ex->getMessage() . '</div>';
     echo '<pre>' . $ex->getTraceAsString() . '</pre>';
+
 
     http_response_code(500);
     exit(1);

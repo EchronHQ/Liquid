13682ada58275e5391c098d84a59f3cb4ed98e32
Stijn Duynslaeger <stijn@echron.com>
2023-11-10 22:00:53 +0000

    Improve Google Tag Manager script rendering and configuration

--- app/code/Liquid/Framework/Output/Html.php
+++ app/code/Liquid/Framework/Output/Html.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace Liquid\Framework\Output;
 
 use MatthiasMullie\Minify\CSS;
+use MatthiasMullie\Minify\JS;
 use voku\helper\HtmlMin;
 
 class Html
@@ -29,6 +30,18 @@ class Html
     {
         $minifier = new CSS();
         $minifier->add($css);
+        return $minifier->minify();
+    }
+
+    /**
+     * https://github.com/matthiasmullie/minify
+     * @param string $js
+     * @return string
+     */
+    public static function minifyJs(string $js): string
+    {
+        $minifier = new JS();
+        $minifier->add($js);
         return $minifier->minify();
     }
 }

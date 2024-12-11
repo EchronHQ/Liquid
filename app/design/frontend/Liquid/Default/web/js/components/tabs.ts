6e8cf01d6ce95cd2f3373f9e0a824e4671d4b97f
Stijn Duynslaeger <stijn@echron.com>
2024-12-11 15:03:44 +0000

    Improve tab component and responsive styling

--- app/design/frontend/Liquid/Default/web/js/components/tabs.ts
+++ app/design/frontend/Liquid/Default/web/js/components/tabs.ts
@@ -11,35 +11,35 @@ export default class Tabs {
         const elements = this.element.find('.tab-content .tab-panel');
 
 
-        let width: number = 0;
-        let height: number = 0;
-        let maxWidth: number | undefined = this.element.innerWidth();
-        if (maxWidth === undefined) {
-            maxWidth = 500;
-        }
-        console.log(maxWidth);
-        elements.each(() => {
-            let widthE: number | undefined = elements.width();
-            if (widthE !== undefined && widthE > maxWidth) {
-                widthE = maxWidth;
-            }
-            const heightE: number | undefined = elements.height();
-
-            if (widthE !== undefined && (width === 0 || widthE > width)) {
-                width = widthE;
-            }
-            if (heightE !== undefined && (height === 0 || heightE > height)) {
-                height = heightE;
-            }
-        })
+        // let width: number = 0;
+        // let height: number = 0;
+        // let maxWidth: number | undefined = this.element.innerWidth();
+        // if (maxWidth === undefined) {
+        //     maxWidth = 500;
+        // }
+        // console.log('Max width ' + maxWidth);
+        // elements.each(() => {
+        //     let widthE: number | undefined = elements.width();
+        //     if (widthE !== undefined && widthE > maxWidth) {
+        //         widthE = maxWidth;
+        //     }
+        //     const heightE: number | undefined = elements.height();
+        //
+        //     if (widthE !== undefined && (width === 0 || widthE > width)) {
+        //         width = widthE;
+        //     }
+        //     if (heightE !== undefined && (height === 0 || heightE > height)) {
+        //         height = heightE;
+        //     }
+        // })
         // a const width: number | undefined = this.element.find('.tab-content').width();
         // const height: number | undefined = this.element.find('.tab-content').height();
 
 
-        if (width === 0 || height === 0) {
-            throw new Error('Unable to load tabs: dimension not defined');
-        }
-        this.element.find('.tab-content').css('width', Math.round(width)).css('height', Math.round(height));
+        // if (width === 0 || height === 0) {
+        //     throw new Error('Unable to load tabs: dimension not defined');
+        // }
+        //  this.element.find('.tab-content').css('width', Math.round(width)).css('height', Math.round(height));
 
         this.element.find('.tab-content .tab-panel').hide();
         this.element.find('.tab-list .tab').on('click', (evt: ClickEvent): void => {

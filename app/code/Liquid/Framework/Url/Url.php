cd9852e59b8cc9c7720fec0900ba7786c6f5e5ed
Stijn Duynslaeger <stijn@echron.com>
2025-11-08 14:42:16 +0000

    Add getters to URL object

--- app/code/Liquid/Framework/Url/Url.php
+++ app/code/Liquid/Framework/Url/Url.php
@@ -8,11 +8,49 @@ class Url
     public string $scheme;
     public string $host;
     public int|null $port = null;
-
     public string|null $user;
     public string|null $password = null;
-
     public string $path;
     public array $query;
     public string $fragments;
+
+    public function getScheme(): string
+    {
+        return $this->scheme;
+    }
+
+    public function getHost(): string
+    {
+        return $this->host;
+    }
+
+    public function getPort(): ?int
+    {
+        return $this->port;
+    }
+
+    public function getUser(): ?string
+    {
+        return $this->user;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function getPath(): string
+    {
+        return $this->path;
+    }
+
+    public function getQuery(): array
+    {
+        return $this->query;
+    }
+
+    public function getFragments(): string
+    {
+        return $this->fragments;
+    }
 }
